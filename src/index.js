7f1374424d4c2b99eb2a38332864b08d5bd14597
leoale95 <131513332+leoale95@users.noreply.github.com>
2023-09-28 13:07:25 +1000

    3-Database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const app = require('./app')
+require('dotenv').config();
+const app = require('./app');
+const { connectToDatabase } = require('./database');
 
-async function main() {
-    await app.listen(8080);
-    console.log('Server on port 8080');
+async function startServer() {
+  await connectToDatabase();
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+  });
 }
+
+startServer();
