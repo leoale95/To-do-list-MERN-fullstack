8484588d6190c2eb8030c4dcc6182d4326cdbf70
leoale95 <131513332+leoale95@users.noreply.github.com>
2023-09-30 11:53:23 +1000

    5-express-routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+// routes
+const NotesRoutes = require('./routes/notes.routes')
+const UserRoutes = require('./routes/users.routes')
 
 
 
@@ -9,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 //Routes
-app.use('/api/users',)
-app.use('api/notes',)
+app.use('/api/users', UserRoutes)
+app.use('api/notes', NotesRoutes)
 
 module.exports = app
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,4 +1,13 @@
 const {Router} = require('express')
 const router = Router() 
 
+router.route('/')
+    .get((req, res) => res.send('User routes'))
+    .post((req, res) => res.send('User routes'))
+
+router.route('/:id')
+    .get((req, res) => res.send('User routes'))
+    .put((req, res) => res.send('User routes'))
+    .delete((req, res) => res.send('User routes'))
+
 module.exports = router
